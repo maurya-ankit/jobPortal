21fa472880b20786bf9540d0b8a4a2e14f05b8f0
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-10-20 04:14:34 +0530

    Some Issues Fixed

--- main/views.py
+++ main/views.py
@@ -9,26 +9,13 @@ from .forms import Fillup_Form
 
 # Create your views here.
 employments = ['part time jobs', 'full time jobs', 'remote jobs', 'internships', 'contract', 'training']
-seniority = ['Student level', 'Entry level', 'mid level', 'Senior level', 'directors', 'VP and above']
-salary_ranges = ['$700-$1000', '$1000-$1200', '$1200-$1400', '$1500-$1800', '$2000-$3000']
+no_of_position = ['0-10', '10', '20', '50', '100', '500', '1000']
 lst = [i for i in range(100)]
 
 
 def homePage(request):
     order = request.GET.get('order') if request.GET.get('order') is not None else 'id'
     page = int(request.GET.get('page'))
-
-    try:
-        part_time_jobs = request.POST.get("part time jobs")
-        full_time_jobs = request.POST.get("full time jobs")
-        remote_jobs = request.POST.get("remote jobs")
-        internships = request.POST.get("internships")
-        contract = request.POST.get("contract")
-        training = request.POST.get("training")
-        seniority_level = request.POST.get('seniority-label')
-        salary_rangesre = request.POST.get('salary-ranges')
-    except:
-        pass
 
     jobsall = JobList.objects.all().order_by(order)
     item_per_page = 12
@@ -38,8 +25,7 @@ def homePage(request):
     paginator = Paginator(jobsall, item_per_page)
     jobs = paginator.get_page(page)
     context = {'employments': employments,
-               'seniority': seniority,
-               'salary_ranges': salary_ranges,
+               'no_of_position': no_of_position,
                'lst': lst,
                'jobs': jobs,
                'start': start,
@@ -69,8 +55,8 @@ class fillupForm(View):
     @method_decorator(login_required)
     def get(self, request, uniq_id):
         form = Fillup_Form()
-        job=JobList.objects.filter(id=uniq_id)[0]
-        context = {'form': form,'job':job}
+        job = JobList.objects.filter(id=uniq_id)[0]
+        context = {'form': form, 'job': job}
         return render(request, 'main/fillupform.html', context)
 
     @method_decorator(login_required)
