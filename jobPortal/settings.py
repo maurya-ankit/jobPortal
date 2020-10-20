b62d086d5dbc00b4d06d5cba060ce79b0e5c4942
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-10-20 16:43:30 +0530

    Added heroku middleware

--- jobPortal/settings.py
+++ jobPortal/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -163,5 +165,5 @@ ACCOUNT_LOGOUT_ON_GET = True
 ACCOUNT_LOGOUT_REDIRECT_URL = '/loggedOut'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
--- jobPortal/wsgi.py
+++ jobPortal/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jobPortal.settings')
 
 application = get_wsgi_application()
+
+from whitenoise.django import
+application = DjangoWhiteNoise(application)
